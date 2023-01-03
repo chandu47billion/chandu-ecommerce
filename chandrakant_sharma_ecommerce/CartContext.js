b96e1a4e6817f4f7f8888d1f50078f6795c5dddf
chandu47billion <chandrakants514@gmail.com>
2023-01-03 14:17:47 +0530

    Function added to remove item from cart

--- chandrakant_sharma_ecommerce/CartContext.js
+++ chandrakant_sharma_ecommerce/CartContext.js
@@ -1,4 +1,4 @@
-import React, {createContext, useState, useEffect} from 'react';
+import React, { createContext, useState, useEffect } from 'react';
 import axios from 'axios';
 export const CartContext = createContext();
 export function CartProvider(props) {
@@ -7,45 +7,79 @@ export function CartProvider(props) {
 
   useEffect(() => {
     axios.get('https://my-json-server.typicode.com/benirvingplt/products/products').then((response) => {
-     console.log(response.data);
-     setProducts(response.data);
-   });
-     });
+      console.log(response.data);
+      setProducts(response.data);
+    });
+  });
 
-     /**
-      * function for add item in cart
-      * @param {*} id 
-      */
+  /**
+   * function for add item in cart
+   * @param {*} id 
+   */
   function addItemToCart(id) {
     const product = products.find((product) => (product.id == id));
     setItems((prevItems) => {
       const item = prevItems.find((item) => (item.id == id));
-      if(!item) {
-          return [...prevItems, {
-              id,
-              qty: 1,
-              product,
-              totalPrice: product.price 
-          }];
+      if (!item) {
+        return [...prevItems, {
+          id,
+          qty: 1,
+          product,
+          totalPrice: product.price
+        }];
       }
-      else { 
-          return prevItems.map((item) => {
-            if(item.id == id) {
-              item.qty++;
-              item.totalPrice += product.price;
-            }
-            return item;
-          });
+      else {
+        return prevItems.map((item) => {
+          if (item.id == id) {
+            item.qty++;
+            item.totalPrice += product.price;
+          }
+          return item;
+        });
       }
     });
-}
+  }
 
-/**
- * function to get item count
- * @returns 
- */
-function getItemsCount() {
-      return items.reduce((sum, item) => (sum + item.qty), 0);
+  /**
+   * function to decrease item count
+   * @param {*} id 
+   */
+  function decreaseItemCountFromCart(id) {
+    const product = products.find((product) => (product.id == id));
+    setItems((prevItems) => {
+      const item = prevItems.find((item) => (item.id == id));
+      if (item) {
+        return prevItems.map((item) => {
+          if (item.id == id) {
+            if (item.qty == 1) {
+              return item;
+            }
+            item.qty--;
+            item.totalPrice -= product.price;
+          }
+          return item;
+        });
+      }
+    });
+  }
+
+  /**
+   * function to remove item from cart
+   * @param {*} id 
+   */
+  function removeItemFromCart(id) {
+    setItems((prevItems) => {
+      const item = prevItems.filter((item) => (item.id !== id));
+      return [...item];
+    });
+  }
+
+  /**
+   * function to get item count
+   * @returns 
+   */
+  function getItemsCount() {
+    return items.reduce((sum, item) => (sum + item.qty), 0);
   }
 
   /**
@@ -53,12 +87,12 @@ function getItemsCount() {
    * @returns 
    */
   function getTotalPrice() {
-      return items.reduce((sum, item) => (sum + item.totalPrice), 0);
-  }  
+    return items.reduce((sum, item) => (sum + item.totalPrice), 0);
+  }
 
   return (
-    <CartContext.Provider 
-      value={{products, items, setItems, getItemsCount, addItemToCart, getTotalPrice}}>
+    <CartContext.Provider
+      value={{ products, items, setItems, getItemsCount, addItemToCart, decreaseItemCountFromCart, removeItemFromCart, getTotalPrice }}>
       {props.children}
     </CartContext.Provider>
   );
