77ea18f330c5f3ed7045ea5aed00401115523f5b
chandu47billion <chandrakants514@gmail.com>
2023-01-03 14:55:41 +0530

    Test use case updated

--- chandrakant_sharma_ecommerce/__tests__/screens/ProductDetails.test.js
+++ chandrakant_sharma_ecommerce/__tests__/screens/ProductDetails.test.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { CartContext } from '../../CartContext';
 import { ProductDetails } from '../../screens/ProductDetails';
+import { shallow } from 'enzyme';
 
 test('renders correctly', () => {
-  const tree = renderer.create(<ProductDetails />).toJSON();
-  expect(tree).toMatchSnapshot();
+  const mockedParams = {
+    route: { params: { productId: 1 } },
+    navigation: ''
+  };
+  jest.spyOn(React, 'useContext').mockImplementation(() => ({
+    name: 'this is a mock context return value'
+  }));
+
+  const myComponent = shallow(
+    <ProductDetails
+    {...mockedParams}
+    />).toJSON();
+  
+  const tree = renderer.create(<ProductDetails {...mockedParams} />).toJSON();
+  expect(myComponent).toMatchSnapshot();
+  
 });
