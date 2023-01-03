61ddef1b7f4ef1cb90661616d55ed8092614094d
chandu47billion <chandrakants514@gmail.com>
2023-01-03 15:07:41 +0530

    Product list test updated

--- chandrakant_sharma_ecommerce/__tests__/screens/ProductList.test.js
+++ chandrakant_sharma_ecommerce/__tests__/screens/ProductList.test.js
@@ -3,6 +3,14 @@ import renderer from 'react-test-renderer';
 import { ProductsList } from '../../screens/ProductsList';
 
 test('renders correctly', () => {
-  const tree = renderer.create(<ProductsList />).toJSON();
+  const mockedParams = {
+    products: [],
+  };
+
+  jest.spyOn(React, 'useContext').mockImplementation(() => ({
+    name: 'this is a mock context return value'
+  }));
+  
+  const tree = renderer.create(<ProductsList {... mockedParams}/>).toJSON();
   expect(tree).toMatchSnapshot();
 });
