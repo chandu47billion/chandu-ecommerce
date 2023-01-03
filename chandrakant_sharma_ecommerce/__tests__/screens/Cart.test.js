56bc6c8f407bfadf05a4d9cf0ce7967f8bb650ab
chandu47billion <chandrakants514@gmail.com>
2023-01-03 15:06:07 +0530

    Cart test case updated

--- chandrakant_sharma_ecommerce/__tests__/screens/Cart.test.js
+++ chandrakant_sharma_ecommerce/__tests__/screens/Cart.test.js
@@ -3,6 +3,21 @@ import renderer from 'react-test-renderer';
 import { Cart } from '../../screens/Cart';
 
 test('renders correctly', () => {
-  const tree = renderer.create(<Cart />).toJSON();
+  const mockedParams = {
+    route: { params: { productId: 1 } },
+    navigation: '',
+    items: [],
+    addItemToCart: jest.fn(),
+    decreaseItemCountFromCart: jest.fn(),
+    removeItemFromCart: jest.fn(),
+    getTotalPrice: jest.fn(),
+    navigation: '',
+  };
+
+  jest.spyOn(React, 'useContext').mockImplementation(() => ({
+    name: 'this is a mock context return value'
+  }));
+  
+  const tree = renderer.create(<Cart {...mockedParams} />).toJSON();
   expect(tree).toMatchSnapshot();
 });
