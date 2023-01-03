3877055eb7e00d94ed7b0026e9bdb338d2ea5e8c
chandu47billion <chandrakants514@gmail.com>
2023-01-03 15:06:58 +0530

    Product detail test updated

--- chandrakant_sharma_ecommerce/__tests__/screens/ProductDetails.test.js
+++ chandrakant_sharma_ecommerce/__tests__/screens/ProductDetails.test.js
@@ -7,18 +7,16 @@ import { shallow } from 'enzyme';
 test('renders correctly', () => {
   const mockedParams = {
     route: { params: { productId: 1 } },
-    navigation: ''
+    navigation: '',
+    addItemToCart: jest.fn(),
+    products: [],
   };
+  
   jest.spyOn(React, 'useContext').mockImplementation(() => ({
     name: 'this is a mock context return value'
   }));
 
-  const myComponent = shallow(
-    <ProductDetails
-    {...mockedParams}
-    />).toJSON();
-  
   const tree = renderer.create(<ProductDetails {...mockedParams} />).toJSON();
-  expect(myComponent).toMatchSnapshot();
+  expect(tree).toMatchSnapshot();
   
 });
