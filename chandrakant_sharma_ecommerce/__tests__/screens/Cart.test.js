e23f9034292b97d77496314756433400b9db5c1d
chandu47billion <chandrakants514@gmail.com>
2023-01-04 12:08:11 +0530

    Unit test case added for cart screen

--- chandrakant_sharma_ecommerce/__tests__/screens/Cart.test.js
+++ chandrakant_sharma_ecommerce/__tests__/screens/Cart.test.js
@@ -1,23 +1,59 @@
 import React from 'react';
-import renderer from 'react-test-renderer';
 import { Cart } from '../../screens/Cart';
+import { render } from '@testing-library/react-native';
+import { CartContext } from '../../CartContext';
 
-test('renders correctly', () => {
-  const mockedParams = {
-    route: { params: { productId: 1 } },
-    navigation: '',
-    items: [],
-    addItemToCart: jest.fn(),
-    decreaseItemCountFromCart: jest.fn(),
-    removeItemFromCart: jest.fn(),
-    getTotalPrice: jest.fn(),
-    navigation: '',
-  };
+test('component handles cart screen', () => {
 
-  jest.spyOn(React, 'useContext').mockImplementation(() => ({
-    name: 'this is a mock context return value'
-  }));
-  
-  const tree = renderer.create(<Cart {...mockedParams} />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
+  const items = [
+    {
+      "id": 1,
+      "colour": "Black",
+      "name": "Black Sheet Strappy Textured Glitter Bodycon Dress",
+      "price": 10,
+      "img": "http://cdn-img.prettylittlething.com/9/0/a/a/90aa90903a135ee59594f47c7685aa7ef3046e44_cly8063_1.jpg?imwidth=1024"
+    },
+    {
+      "id": 2,
+      "colour": "Stone",
+      "name": "Stone Ribbed Strappy Cut Out Detail Bodycon Dress",
+      "price": 4,
+      "img": "https://cdn-img.prettylittlething.com/3/6/5/a/365a5d1dce6a2b77b564379b302c9d83afccf33b_cmd2051_1.jpg?imwidth=1024"
+    },
+    {
+      "id": 3,
+      "colour": "Black",
+      "name": "Black Frill Tie Shoulder Bodycon Dress",
+      "price": 7.99,
+      "img": "https://cdn-img.prettylittlething.com/d/c/3/3/dc337260f9ecefdb99a8c8e98cd73ccb1b79cea5_cmb6804_4.jpg?imwidth=1024"
+    },
+    {
+      "id": 5,
+      "colour": "Red",
+      "name": "Red Pin Stripe Belt T Shirt Dress",
+      "price": 17,
+      "img": "https://cdn-img.prettylittlething.com/f/7/1/8/f718a4011ddf92f48aeefff6da0f475178694599_cly0842_1.jpg?imwidth=1024"
+    }
+  ];
+  const addItemToCart = jest.fn();
+  const decreaseItemCountFromCart = jest.fn();
+  const removeItemFromCart = jest.fn();
+  const getTotalPrice = jest.fn();
+
+  const { getAllByText } = render(
+    <CartContext.Provider
+      value={{ items, addItemToCart, decreaseItemCountFromCart, removeItemFromCart, getTotalPrice }}>
+      <Cart />
+    </CartContext.Provider>
+  );
+
+  const incrementbutton = getAllByText('Increment Count');
+  const decrementbutton = getAllByText('Decrement Count');
+  const removeThisItembutton = getAllByText('Remove this Item');
+  const grandTotalButton = getAllByText('Grand Total:');
+  expect(incrementbutton).toBeDefined();
+  expect(decrementbutton).toBeDefined();
+  expect(removeThisItembutton).toBeDefined();
+  expect(grandTotalButton).toBeDefined();
+}
+);
